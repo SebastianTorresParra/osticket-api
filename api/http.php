7be05a8455b95e98e48fce4cda78fa27afd29e09
juan sebastian torres <jstorres649@misena.edu.co>
2020-10-14 14:35:50 -0500

    Eliminacion de funciones y rutas no usadas, adicion de loader en la vista de consulta de tickets

--- api/http.php
+++ api/http.php
@@ -15,6 +15,11 @@
 **********************************************************************/
 // Use sessions — it's important for SSO authentication, which uses
 // /api/auth/ext
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
 define('DISABLE_SESSION', false);
 
 require 'api.inc.php';
@@ -23,17 +28,10 @@ require 'api.inc.php';
 require_once INCLUDE_DIR."class.dispatcher.php";
 
 $dispatcher = patterns('',
-        url_post("^/tickets\.(?P<format>xml|json|email)$", array('api.tickets.php:TicketApiController','create')),
-        
-            // RESTful
-            url_get("^/tickets$", array('api.tickets.php:TicketApiController','restGetTickets')),
-            // url_get("^/tickets/(?P<ticket_number>\d{6})$", array('api.tickets.php:TicketApiController','restGetTicket')),
-            url_get("^/tickets/ticketInfo$", array('api.tickets.php:TicketApiController','getTicketInfo')),
-            url_get("^/tickets/staffTickets$", array('api.tickets.php:TicketApiController','getStaffTickets')),
+            url_post("^/tickets\.(?P<format>xml|json|email)$", array('api.tickets.php:TicketApiController','create')),
+            // consulta de ticket por usuario
             url_get("^/tickets/clientTickets$", array('api.tickets.php:TicketApiController','getClientTickets')),
-            # Should stay disabled until there's an api key permission for ticket deletion
-            #url_delete("^/tickets/(?P<ticket_number>\d{6})$",
-        );
+);
 
 Signal::send('api', $dispatcher);
 
